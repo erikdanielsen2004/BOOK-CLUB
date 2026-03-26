ce85d3278e41155dcdc1775a1a4e94969ff22541
Diego MB <116322695+DiegoJMB@users.noreply.github.com>
2026-03-26 16:09:30 -0400

    changing import to require

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,7 @@
-import express from "express";
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
-import User from "../models/User.js";
+const express require("express");
+const bcrypt require("bcryptjs");
+const jwt require("jsonwebtoken");
+const User require("../models/User.js");
 
 const router = express.Router();
 
