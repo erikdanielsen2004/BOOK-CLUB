0f1851cad9837fc4b96a522bddfc8e9edfa2c776
Diego MB <116322695+DiegoJMB@users.noreply.github.com>
2026-03-26 16:50:01 -0400

    debug stuff

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,13 @@
-const createError = require('http-errors');
 const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+/*
+const createError = require('http-errors');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const mongoose = require('mongoose');
-const app = express();
-
+*/
 const authRouter = require('./routes/auth.js');
-app.use(express.json());
 
 const mongoURI = 'no peeking';
 
@@ -19,14 +19,14 @@ mongoose.connect(mongoURI)
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
+// app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/auth', authRouter);
-
+/*
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
@@ -42,5 +42,5 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
+*/
 module.exports = app;
