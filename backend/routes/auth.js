729ee359b5c21d2819d4d93edd643582e324638d
Francisco Arisso <frankiearisso721@gmail.com>
2026-03-27 11:58:01 -0400

    Potential Login fix?

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -72,7 +72,7 @@ router.post('/signup', async (req, res) => {
     }
 });
 
-router.get('/login', async (req, res) => {
+router.post('/login', async (req, res) => {
     try {
         const { email, passwordHash } = req.body;
         if (!email || !passwordHash) return res.status(400).json({ message: "Cannot have empty fields"})
