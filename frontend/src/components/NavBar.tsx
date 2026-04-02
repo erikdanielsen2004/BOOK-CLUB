7770564c0c46cc5aba591c0ccbe80af5594f64c0
amzuleta <158415897+amzuleta@users.noreply.github.com>
2026-04-02 08:56:58 -0400

    Refactor NavBar component and update styles import

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,21 +1,16 @@
-import styles from './NavBar.module.css';
+import { useNavigate } from 'react-router-dom';
+import styles from '../styles/NavBar.module.css';
 
-function NavBar()
-{
-  function doLogin(event:any) : void
-  {
-    event.preventDefault();
+function NavBar() {
+  const navigate = useNavigate();
 
-    alert('doIt()');
-  }
-
-    return(
-           <div id="navDiv" className={styles.navContainer}>
-           <button className={`${styles.btn} ${styles.about}`}>About Us</button>
-        <button className={styles.btn}>Log In</button>
-           <button className={styles.btn2}>Sign Up</button>
-     </div>
-    );
-};
+  return (
+    <div id="navDiv" className={styles.navContainer}>
+      <button className={`${styles.btn} ${styles.about}`}>About Us</button>
+      <button className={styles.btn} onClick={() => navigate('/login')}>Log In</button>
+      <button className={styles.btn2} onClick={() => navigate('/register')}>Sign Up</button>
+    </div>
+  );
+}
 
 export default NavBar;
