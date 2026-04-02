1da8601193cb0911fb930fcf18ab114cbf859d64
amzuleta <158415897+amzuleta@users.noreply.github.com>
2026-04-02 08:50:35 -0400

    Fix import paths in LoginPage component

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,6 +1,6 @@
-import PageTitle from '../components/PageTitle.tsx';
-import NavBar from '../components/NavBar.tsx';
-import styles from './LoginPage.module.css';
+import PageTitle from '../components/PageTitle';
+import NavBar from '../components/NavBar';
+import styles from '../styles/LoginPage.module.css';
 import BookThumbnail from '../components/BookThumbnail';
 
 
@@ -10,7 +10,7 @@ const LoginPage = () =>
     return(
         <div className={styles.wrapper}>
            <NavBar />
-           
+
            <main className={styles.wrapper2}>
             <PageTitle />
             <BookThumbnail />
