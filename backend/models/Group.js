3ddd5bc8dc2b827990ae0dcf91fb48f21495db00
Francisco Arisso <66758179+franciscoarisso@users.noreply.github.com>
2026-04-02 16:19:13 -0400

    Add voting functionality to group schema

--- backend/models/Group.js
+++ backend/models/Group.js
@@ -1,36 +1,51 @@
-
 const mongoose = require('mongoose');
 
-const groupSchema = new mongoose.Schema(
-    {
-
-        name: {
-            type: String,
-            required: true
-        },
-        description: {
-            type: String
-        },
-        owner: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'User',
-            required: true
-        },
-        members: [{
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'User',
-            required: true
-        }],
-        currentBook: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Book'
-        },
-        bookCandidates: [{
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Book'
-        }]
-
+const voteSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    },
+    book: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Book',
+      required: true
     }
+  },
+  { _id: true, timestamps: true }
+);
+
+const groupSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    },
+    members: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    }],
+    currentBook: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Book'
+    },
+    bookCandidates: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Book'
+    }],
+    votes: [voteSchema]
+  },
+  { timestamps: true }
 );
 
 module.exports = mongoose.model('Group', groupSchema);
