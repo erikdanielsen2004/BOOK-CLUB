20a06d5d8d7a127474608aeb4b87778c7b1eeda4
Corday Davis <yadroc.sivad@gmail.com>
2026-04-02 23:23:56 -0400

    attempted to fix books cover thumbnails...
    not going very well. covers show up but there is a chance the covers show up as no image available

--- frontend/src/components/BookThumbnail.tsx
+++ frontend/src/components/BookThumbnail.tsx
@@ -9,14 +9,25 @@ function BookThumbnail() {
 
       if (books.length > 0) return;
 
-      const apiKey = import.meta.env.VITE_BOOKS_API_KEY;
-      const url = `https://www.googleapis.com/books/v1/volumes?q=subject:fiction&key=${apiKey}&maxResults=3`;
+        const randomOffset = Math.floor(Math.random() * 150);
+        
+      const apiKey = process.env.REACT_APP_BOOKS_API_KEY;
+      const url = `https://www.googleapis.com/books/v1/volumes?q=subject:fiction&startIndex=${randomOffset}&key=${apiKey}&maxResults=40`;
 
       fetch(url)
         .then(res => res.json())
         .then(data => {
            if (data.items) {
-             setBooks(data.items);
+               
+               const filtered = data.items.filter((book: any) => {
+                         const thumb = book.volumeInfo.imageLinks?.thumbnail;
+                         
+                         return (
+                                 thumb && !thumb.includes("id=mqM") && !thumb.includes("availability") &&
+                                 !thumb.includes("google_books_unknown")
+                                 );
+                       });
+             setBooks(filtered.slice(0,3));
            }
         });
     }, []);
@@ -26,11 +37,11 @@ function BookThumbnail() {
       {books.map((book) => {
         const thumb = book.volumeInfo.imageLinks?.thumbnail;
         if (!thumb) return null;
-
+          const placeholder = '/image.png';
         return (
           <img
             key={book.id}
-            src={thumb.replace('http://', 'https://')}
+            src={thumb ? thumb.replace('http://', 'https://').replace('&zoom=1','&zoom=2') : placeholder}
             alt={book.volumeInfo.title}
             className={styles.bookCover}
           />
