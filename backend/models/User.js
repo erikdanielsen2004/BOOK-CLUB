42ddecbe949e7ebdb9001026a40d0e0ecc343eea
Diego MB <116322695+DiegoJMB@users.noreply.github.com>
2026-04-07 15:00:11 -0400

    renaming password field

--- backend/models/User.js
+++ backend/models/User.js
@@ -19,7 +19,7 @@ const userSchema = new mongoose.Schema(
             trim: true,
             lowercase: true
         },
-        passwordHash: {
+        password: {
             type: String,
             required: true
         },
