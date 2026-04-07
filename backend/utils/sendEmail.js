6b4ff3131afe2fdcb6367ddc0fea79b569061506
Diego MB <116322695+DiegoJMB@users.noreply.github.com>
2026-04-07 18:29:43 -0400

    Update sendEmail.js

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -10,8 +10,8 @@ const sendVerificationEmail = async (email, token) => {
   const transporter = nodemailer.createTransport({
     service: 'gmail',
     host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
+    port: 587,
+    secure: false,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
