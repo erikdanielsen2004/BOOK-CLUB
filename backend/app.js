7b1bf3a3b691203ba73cde1ad0f9eddb9c59529e
Diego MB <116322695+DiegoJMB@users.noreply.github.com>
2026-04-07 15:47:32 -0400

    using environmental variables

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const authRouter = require('./routes/auth.js');
 // const userBooksRouter = require('./routes/userBooks.js');
 const groupMainRouter = require('./routes/groupMain.js');
 
-const mongoURI = 'no peeking';
+const mongoURI = process.env.MONGO_URI;
 mongoose.connect(mongoURI)
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.error('MongoDB connection error:', err));
@@ -20,7 +20,7 @@ app.use('/api/auth', authRouter);
 // app.use('/api/user-books', userBooksRouter);
 app.use('/api/group-main', groupMainRouter);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 module.exports = app;
