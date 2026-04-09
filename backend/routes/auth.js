4af80eea7dc115bd33990829b0e2a976d40ed598
Diego MB <116322695+DiegoJMB@users.noreply.github.com>
2026-04-09 15:38:11 -0400

    fixes for verify error

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -50,7 +50,7 @@ router.post("/signup", async (req, res) => {
             { expiresIn: process.env.JWT_EMAIL_VER_EXPIRES_IN }
         );
 
-        await sendVerificationEmail(user.email, verificationToken).catch((error) => console.error("Email send failed.", error));
+        await sendVerificationEmail(user.email, verificationToken);
 
         return res.status(201).json({
             message: "User registered successfully. Please check your email to verify your account.",
