2f575139f77bbb28e464bfd8b2c5c19528879edf
Francisco Arisso <66758179+franciscoarisso@users.noreply.github.com>
2026-04-12 00:28:44 -0400

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    cssCodeSplit: true,
+    sourcemap: false,
+    chunkSizeWarningLimit: 700,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules/react') || id.includes('node_modules/react-dom')) {
+            return 'react-vendor'
+          }
+
+          if (id.includes('node_modules/react-router-dom')) {
+            return 'router'
+          }
+
+          if (id.includes('@fontsource/pt-mono')) {
+            return 'fonts'
+          }
+        }
+      }
+    }
+  }
 })
