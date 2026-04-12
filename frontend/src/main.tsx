d317eaa8e89334c3f1dec2a9248c7c9d1f8f8b86
Francisco Arisso <66758179+franciscoarisso@users.noreply.github.com>
2026-04-12 00:29:27 -0400

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,8 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import "@fontsource/pt-mono"; 
+import "@fontsource/pt-mono";
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <App />
 )
